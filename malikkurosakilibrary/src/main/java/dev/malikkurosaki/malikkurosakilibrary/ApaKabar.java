0f14efd76d343c09b6e6737a39697a4d46e0b02e
malikkurosaki <kurosakiblackangel@gmail.com>
2019-05-12 13:46:48 +0800

    public

--- malikkurosakilibrary/src/main/java/dev/malikkurosaki/malikkurosakilibrary/ApaKabar.java
+++ malikkurosakilibrary/src/main/java/dev/malikkurosaki/malikkurosakilibrary/ApaKabar.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ApaKabar {
 
-    ApaKabar(Context context){
+    public ApaKabar(Context context){
         Toast.makeText(context.getApplicationContext(),"apa kabar",Toast.LENGTH_LONG).show();
     }
 }
